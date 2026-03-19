82f911c97836358057795f6ee8ddb5e355f216e9
nxivm-bots <cyrilbinu2006@gmail.com>
2026-03-19 17:41:35 +0530

    Update telegram_uploader.py

--- bot/helper/mirror_leech_utils/telegram_uploader.py
+++ bot/helper/mirror_leech_utils/telegram_uploader.py
@@ -133,14 +133,14 @@ class TelegramUploader:
     
     async def _prepare_file(self, file_, dirpath):
         filename = ospath.splitext(file_)[0]
-        cap_mono = f"<code>{filename}.𝖯𝗈𝗐𝖾𝗋𝖾𝖽.𝖻𝗒↝🍀ᗷƖ໐໐ძ'ѕˊˎ-</code>"
+        f"@team_bloods"
         if self._lprefix:
             self._lprefix = re_sub("<.*?>", "", self._lprefix)
             new_path = ospath.join(dirpath, f"{self._lprefix} {file_}")
             await rename(self._up_path, new_path)
             self._up_path = new_path
         else:
-            cap_mono = f"<code>{filename}.𝖯𝗈𝗐𝖾𝗋𝖾𝖽.𝖻𝗒↝🍀ᗷƖ໐໐ძ'ѕˊˎ-</code>"
+            f"@team_bloods"
         if len(file_) > 60:
             if is_archive(file_):
                 name = get_base_name(file_)
